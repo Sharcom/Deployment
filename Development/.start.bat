be34a7c6c0a75404a7a16ace8832602f8acf8507
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-01-07 21:10:05 +0100

    Dockerbuild relative paths

--- Development/.start.bat
+++ Development/.start.bat
@@ -1,4 +1,2 @@
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f autoscaler.yaml
+kubectl apply -f deployment.yaml | kubectl apply -f service.yaml | kubectl apply -f autoscaler.yaml
